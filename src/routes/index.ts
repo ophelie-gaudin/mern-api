19a3545d0117b04b1937de2b0c856b9c3579bbdb
Ophélie <ophelie.gaudin@outlook.fr>
2022-05-04 16:44:41 +0200

    add cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import { userRouter } from './user.routes';
 import { postRouter } from './post.routes';
@@ -10,6 +11,17 @@ import { checkUser, requireAuth } from '../middleware/auth.middleware';
 export const initRouter = () => {
   const app: Express = express();
   const port = process.env.PORT;
+
+  const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    allowedHeaders: ['sessionId', 'Content-Type'],
+    exposedHeaders: ['sessionId'],
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    preflightContinue: false,
+  };
+
+  app.use(cors(corsOptions));
 
   app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server !!!!!');
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+// import fs from 'fs';
 import { Router } from 'express';
 import multer from 'multer';
 
